d2095aefe19232387973884974379149aa62fa48
oktonion <0oktonion0@gmail.com>
2025-10-30 09:45:27 +0300

    Update testsuit.h

--- tests/service/testsuit.h
+++ tests/service/testsuit.h
@@ -43,39 +43,39 @@
 #include ANGELSCRIPT_LIB_SRC_H_PATH(as_scriptengine)
 #include ANGELSCRIPT_SCRIPTBUILDER_H_PATH
 
-#define SERVICE_FILE_TRANSLATION_YYMMDD_DATE(name)                                         \
-    const char name[] =                                                                    \
-    {                                                                                      \
-        /* YY year */                                                                      \
-        __DATE__[9], __DATE__[10],                                                         \
-                                                                                           \
-        /* First month letter, Oct Nov Dec = '1' otherwise '0' */                          \
-        (__DATE__[0] == 'O' || __DATE__[0] == 'N' || __DATE__[0] == 'D') ? '1' : '0',      \
-                                                                                           \
-        /* Second month letter */                                                          \
-        (__DATE__[0] == 'J') ? ((__DATE__[1] == 'a') ? '1' :       /* Jan, Jun or Jul */   \
-                                 ((__DATE__[2] == 'n') ? '6' : '7')) :                     \
-        (__DATE__[0] == 'F') ? '2' :                                /* Feb */              \
-        (__DATE__[0] == 'M') ? (__DATE__[2] == 'r') ? '3' : '5' :   /* Mar or May */       \
-        (__DATE__[0] == 'A') ? (__DATE__[1] == 'p') ? '4' : '8' :   /* Apr or Aug */       \
-        (__DATE__[0] == 'S') ? '9' :                                /* Sep */              \
-        (__DATE__[0] == 'O') ? '0' :                                /* Oct */              \
-        (__DATE__[0] == 'N') ? '1' :                                /* Nov */              \
-        (__DATE__[0] == 'D') ? '2' :                                /* Dec */              \
-        0,                                                                                 \
-                                                                                           \
-        /* First day letter, replace space with digit */                                   \
-        __DATE__[4] == ' ' ? '0' : __DATE__[4],                                            \
-                                                                                           \
-        /* Second day letter */                                                            \
-        __DATE__[5],                                                                       \
-                                                                                           \
-       '\0'                                                                                \
+#define SERVICE_FILE_TRANSLATION_YYMMDD_DATE(name)                                           \
+    const char name[] =                                                                      \
+    {                                                                                        \
+        /* YY year */                                                                        \
+        *(__DATE__+9), *(__DATE__+10),                                                       \
+                                                                                             \
+        /* First month letter, Oct Nov Dec = '1' otherwise '0' */                            \
+        (*(__DATE__+0) == 'O' || *(__DATE__+0) == 'N' || *(__DATE__+0) == 'D') ? '1' : '0',  \
+                                                                                             \
+        /* Second month letter */                                                            \
+        (*(__DATE__+0) == 'J') ? ((*(__DATE__+1) == 'a') ? '1' :       /* Jan, Jun or Jul */ \
+                                 ((*(__DATE__+2) == 'n') ? '6' : '7')) :                     \
+        (*(__DATE__+0) == 'F') ? '2' :                                /* Feb */              \
+        (*(__DATE__+0) == 'M') ? (*(__DATE__+2) == 'r') ? '3' : '5' :   /* Mar or May */     \
+        (*(__DATE__+0) == 'A') ? (*(__DATE__+1) == 'p') ? '4' : '8' :   /* Apr or Aug */     \
+        (*(__DATE__+0) == 'S') ? '9' :                                /* Sep */              \
+        (*(__DATE__+0) == 'O') ? '0' :                                /* Oct */              \
+        (*(__DATE__+0) == 'N') ? '1' :                                /* Nov */              \
+        (*(__DATE__+0) == 'D') ? '2' :                                /* Dec */              \
+        0,                                                                                   \
+                                                                                             \
+        /* First day letter, replace space with digit */                                     \
+        *(__DATE__+4) == ' ' ? '0' : *(__DATE__+4),                                          \
+                                                                                             \
+        /* Second day letter */                                                              \
+        *(__DATE__+5),                                                                       \
+                                                                                             \
+       '\0'                                                                                  \
     };
 
-#define SERVICE_FILE_TRANSLATION_HHMMSS_TIME(name)                                         \
-    const char name[] = {                                                                  \
-        __TIME__[0], __TIME__[1], __TIME__[3], __TIME__[4], __TIME__[6], __TIME__[7], '\0' \
+#define SERVICE_FILE_TRANSLATION_HHMMSS_TIME(name)                                                     \
+    const char name[] = {                                                                              \
+        *(__TIME__+0), *(__TIME__+1), *(__TIME__+3), *(__TIME__+4), *(__TIME__+6), *(__TIME__+7), '\0' \
     };
 
 #define SERVICE_MESSAGE_CALLBACK_WITH_ASSERTS(name) \
@@ -198,6 +198,10 @@ namespace testsuite {
 
     struct asCScriptEngineHack : asCScriptEngine 
     {
+        virtual int GetMessageCallback(ANGELSCRIPT_NS_QUALIFIER asSFuncPtr* callback, void** obj, ANGELSCRIPT_NS_QUALIFIER asDWORD* callConv)
+        {
+            return asCScriptEngine::GetMessageCallback(callback, obj, callConv);
+        }
         static int GetMessageCallback(ANGELSCRIPT_NS_QUALIFIER asIScriptEngine& asIScriptEngine, ANGELSCRIPT_NS_QUALIFIER asSFuncPtr* callback, void** obj, ANGELSCRIPT_NS_QUALIFIER asDWORD* callConv)
         {
             ANGELSCRIPT_NS_QUALIFIER asCScriptEngine& engine = dynamic_cast<ANGELSCRIPT_NS_QUALIFIER asCScriptEngine&>(asIScriptEngine); // will generate exception with some user provided engine class and that's allright
@@ -265,6 +269,7 @@ namespace testsuite {
                 }
             }
         }
+
     };
 } // namespace testsuite
 
