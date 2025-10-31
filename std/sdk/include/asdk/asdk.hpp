0e4a7ace97410bd6b5b2d51d1c67e9477eba899b
oktonion <0oktonion0@gmail.com>
2025-10-31 08:58:09 +0300

    Update asdk.hpp

--- std/sdk/include/asdk/asdk.hpp
+++ std/sdk/include/asdk/asdk.hpp
@@ -1160,7 +1160,7 @@ namespace asdk {
             template<class OtherT>
             reflect&
             operator_equal_to(const std::string& other_str
-                , ASDK_SFINAE_DEFAULT_FUNCTION_ARG(sizeof((*(const T*)(0)) == (*(const OtherT*)(0)))))
+                , ASDK_SFINAE_DEFAULT_FUNCTION_ARG(sizeof((*(T*)(0)) == (*(OtherT*)(0)))))
             {
                 return operator_equal_to(static_cast<bool(*)(const T&, const OtherT&)>(opEquals), other_str);
             }
@@ -1213,9 +1213,9 @@ namespace asdk {
             operator_compare(const std::string &other_str
                 , ASDK_SFINAE_DEFAULT_FUNCTION_ARG
                 (
-                    sizeof ((*(const T*)(0)) < (*(const OtherT*)(0)))
+                    sizeof ((*(T*)(0)) < (*(OtherT*)(0)))
                     +
-                    sizeof ((*(const T*)(0)) > (*(const OtherT*)(0)))
+                    sizeof ((*(T*)(0)) > (*(OtherT*)(0)))
                 ))
             {
                 return operator_compare(static_cast<int(*)(const T&, const OtherT&)>(opCmp), other_str);
@@ -1269,7 +1269,7 @@ namespace asdk {
             template<class OtherT>
             reflect&
             operator_add(const std::string &other_str
-                , ASDK_SFINAE_DEFAULT_FUNCTION_ARG( sizeof((*(const T*)(0)) + (*(const OtherT*)(0))) ))
+                , ASDK_SFINAE_DEFAULT_FUNCTION_ARG( sizeof((*(T*)(0)) + (*(OtherT*)(0))) ))
             {
                 return operator_add(static_cast<T(*)(const T&, OtherT)>(opAdd), other_str);
             }
@@ -1319,7 +1319,7 @@ namespace asdk {
             template<class OtherT, class ThisT>
             reflect&
             operator_add(const std::string& other_str
-                , ASDK_SFINAE_DEFAULT_FUNCTION_ARG(((*(const OtherT*)(0)) + (*(const ThisT*)(0)))))
+                , ASDK_SFINAE_DEFAULT_FUNCTION_ARG(((*(OtherT*)(0)) + (*(ThisT*)(0)))))
             {
                 return operator_add<T, const OtherT&, const ThisT&>(static_cast<T(*)(const OtherT&, const ThisT&)>(opAdd), name, other_str);
             }
